2ddf045385fc1f8c39cf1f8274379a5dc37033c8
Amr <82913751+Amr-AG@users.noreply.github.com>
2022-09-17 15:22:21 +0200

    Update learn.js

--- JS/learn.js
+++ JS/learn.js
@@ -11,36 +11,35 @@ login.addEventListener("click", (e) => {
     password.value.length < 6
   ) {
     e.preventDefault();
+  } else {
+    container.innerHTML = "";
+    let content = document.createElement("div");
+    content.className = "content";
+    content.innerText = "Loading...";
+    let load = document.createElement("div");
+    load.className = "loading";
+
+    let land = document.createElement("div");
+    land.className = "landing";
+
+    container.appendChild(content);
+    container.appendChild(load);
+    container.appendChild(land);
+
+    load.style.display = "block";
+    land.style.display = "block";
+
+    setTimeout(() => {
+      let done = document.createElement("div");
+      done.innerHTML = "Welcome";
+      done.className = "done";
+      container.appendChild(done);
+      done.style.cssText =
+        "transition :2s;font-size: 40px;letter-spacing:2px;line-height: 1.8;padding: 31px 65px;border: solid 1px #202020;border: 1px solid #202020;box-shadow: rgb(0 0 0 / 12%) -11px 18px 1px 0px;-webkit-text-stroke-width: medium;";
+      content.style.display = "none";
+      load.style.display = "none";
+      land.style.display = "none";
+      myAudio.play();
+    }, 6000);
   }
-
-  container.innerHTML = "";
-
-  let content = document.createElement("div");
-  content.className = "content";
-  content.innerText = "Loading...";
-  let load = document.createElement("div");
-  load.className = "loading";
-
-  let land = document.createElement("div");
-  land.className = "landing";
-
-  container.appendChild(content);
-  container.appendChild(load);
-  container.appendChild(land);
-
-  load.style.display = "block";
-  land.style.display = "block";
-
-  let trans = setTimeout(() => {
-    let done = document.createElement("div");
-    done.innerHTML = "Welcome";
-    done.className = "done";
-    container.appendChild(done);
-    done.style.cssText =
-      "transition :2s;font-size: 40px;letter-spacing:2px;line-height: 1.8;padding: 31px 65px;border: solid 1px #202020;border: 1px solid #202020;box-shadow: rgb(0 0 0 / 12%) -11px 18px 1px 0px;-webkit-text-stroke-width: medium;";
-    content.style.display = "none";
-    load.style.display = "none";
-    land.style.display = "none";
-    myAudio.play();
-  }, 6000);
 });
